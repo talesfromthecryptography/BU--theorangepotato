e622f410edc18d6e6fcdc11ea8a53b94b711924f
Austin Bohannon <bohan112@mail.chapman.edu>
2019-09-12 12:44:54 -0700

    Update tests to test all implemented functions

--- test_bu.c
+++ test_bu.c
@@ -5,16 +5,24 @@
 
 
 int main() {
-  bigunsigned a,b,c;
-  char s[BU_MAX_HEX+1];
+  bigunsigned a,b,c,d;
 
-  bu_readhex(&a,"CAB51AFFBEEF");
+  printf("Reading in string...\n");
+  bu_readhex(&a,"CAB5 1AFF BEEF");
   bu_readhex(&b,"111111111111");
-
   bu_dbg_printf(&a);
 
+  printf("Adding bigunsigneds...\n");
   bu_add(&c, &a, &b);
- 
   bu_dbg_printf(&c);
+
+  printf("Copying bigunsigned...\n");
+  bu_cpy(&d, &a);
+  bu_dbg_printf(&d);
+
+  printf("Clearing bigunsigned...\n");
+  bu_clear(&d);
+  bu_dbg_printf(&d);
+
   return 0;
 }
